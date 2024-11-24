a679ccbcc40ade9d940705b0a6b315f23eae397f
d-man7562 <domenic.mancuso@maine.edu>
2024-11-24 00:51:41 -0500

    Added sprite animation for player
    Signed-off-by: d-man7562 <domenic.mancuso@maine.edu>

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var speed: float = 500
 var player: Sprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("ready called")
@@ -9,7 +11,7 @@ func _ready() -> void:
 		print("found sprite!")
 	else:
 		print("sprite not found")
-	
+	$AnimatedSprite2D.play("running_default")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
