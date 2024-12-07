93a132bf91f12e474f58a428af1fc09f0e9cef19
d-man7562 <domenic.mancuso@maine.edu>
2024-12-06 20:19:41 -0500

    CHECKPOINT!
    Signed-off-by: d-man7562 <domenic.mancuso@maine.edu>

--- scenes/scripts/Hurtbox.gd
+++ scenes/scripts/Hurtbox.gd
@@ -2,7 +2,11 @@ class_name HurtBox
 extends Area2D
 @onready var player: Node2D = $".."
 @onready var animated_sprite_2d: AnimatedSprite2D = $"../AnimatedSprite2D"
+@onready var boulder_spawner: Node2D = $BoulderSpawner
+@onready var parallax_bg: ParallaxBackground = $"../../ParallaxBackground"
 
+var DEATH: bool = false
+var on:bool = true
 #@onready var parallax_background: ParallaxBackground = $ParallaxBackground
 #@onready var parallax_background = preload("res://scenes/scripts/parallax_background.gd")
 @onready var parallax = get_tree().get_root().get_node("MAIN_LEVEL/ParallaxBackground")
@@ -18,11 +22,27 @@ func _on_area_entered(area: Area2D) -> void:
 		print("Players Hurtbox detect collision with hitbox")
 		collision_detected.emit()
 		animated_sprite_2d.stop()
+		player.can_move = false
+		parallax_bg.scroll_speed = 0
 		await get_tree().create_timer(0.5).timeout
-		
-		#if player.position.x < 360:
+		DEATH = true
+		print(player.position.y)
 		#	
 			
 		#parallax_background.is_scrolling = false
+func _process(delta: float) -> void:
+	var up_force = 400
+	var grav = 440
+	var up_limit = 200
+	if DEATH:
+		if on:#if player.position.x < 360:
+			#print(on)
+			if player.position.y < up_limit:
+				on = false
+			else:
+				player.position.y -= up_force*delta
+		else:
+			player.position.y += grav*delta
+		#print(on)
 func _on_collision() -> void:
 	parallax.is_scrolling = false
--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -2,7 +2,7 @@ extends Node2D
 var speed: float = 500
 var player: Sprite2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var can_move = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("ready called")
@@ -14,10 +14,11 @@ func _ready() -> void:
 	$AnimatedSprite2D.play("running_default")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		position.x += speed * delta
-	if Input.is_action_pressed("ui_left"):
-		position.x -= speed * delta
+	if can_move:	
+		if Input.is_action_pressed("ui_right"):
+			position.x += speed * delta
+		if Input.is_action_pressed("ui_left"):
+			position.x -= speed * delta
 
 	position.x = clampf(position.x, 0,720)
 		
